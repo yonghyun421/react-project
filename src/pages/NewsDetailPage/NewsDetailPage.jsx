391f52e3b253aca42814cfd09d2eacbc353baae0
김효은 <hyoeun@gmail.com>
2024-04-17 22:10:03 +0900

    리액트 작업중

--- src/pages/NewsDetailPage/NewsDetailPage.jsx
+++ src/pages/NewsDetailPage/NewsDetailPage.jsx
@@ -1,6 +1,40 @@
 import React from "react";
+import { Alert } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { useNewsListQuery } from "../../hooks/useNews";
 
-function NewsDetailPage() {
-  return <div>NewsPage</div>;
-}
+const NewsDetailPage = () => {
+  const { id } = useParams();
+  const { data: news, isLoading, isError, error } = useNewsListQuery();
+  // eslint-disable-next-line
+  console.log(news);
+  const extractNumber = url => {
+    const numericPart = url.replace(/\D/g, "");
+    return Number.isNaN(Number(numericPart)) ? null : Number(numericPart);
+  };
+
+  const newsDetail = news?.articles.filter(
+    article => extractNumber(article.url) === parseInt(id, 10),
+  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+  /* eslint-disable react/no-danger */
+  return (
+    <div>
+      {newsDetail.map(article => (
+        <div key={article.url}>
+          <div>{article.title}</div>
+          <div dangerouslySetInnerHTML={{ __html: article.description }} />
+        </div>
+      ))}
+    </div>
+  );
+  /* eslint-disable react/no-danger */
+};
+
 export default NewsDetailPage;
