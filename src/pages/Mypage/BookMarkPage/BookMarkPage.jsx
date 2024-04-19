47460f26822ceb5351842d665ff364ab8f9ad43a
김효은 <hyoeun@gmail.com>
2024-04-19 13:37:54 +0900

    마이페이지 파일 추가

---
+++ src/pages/Mypage/BookMarkPage/BookMarkPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BookMarkPage() {
+  return (
+    <div>
+      BookMarkPage
+      <div>dd</div>
+    </div>
+  );
+}
+
+export default BookMarkPage;
---
+++ src/pages/Mypage/JoinPage/JoinPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function JoinPage() {
+  return (
+    <div>
+      JoinPage
+      <div>JoinPage</div>
+    </div>
+  );
+}
+
+export default JoinPage;
