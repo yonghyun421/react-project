965570c66148df699139c7ffaeb46072480cabef
김효은 <hyoeun@gmail.com>
2024-04-20 16:35:45 +0900

    마이페이지 추가 작업

--- src/pages/Mypage/BookMarkPage/BookMarkPage.jsx
+++ src/pages/Mypage/BookMarkPage/BookMarkPage.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import noImage from "../../../assets/noImage.jpg";
+import "./BookMarkPage.style.css";
 
 function BookMarkPage() {
   return (
     <div className="bookMarkPage--wrap">
-      <p className="bookMarkPage__title">관심분야 설정</p>
-      <ul>
-        <li>dklf</li>
-      </ul>
+      <div className="bookMarkPage--box">
+        <p className="bookMarkPage--title">
+          북마크한 뉴스
+          <span>13</span>
+        </p>
+        <ul className="bookMarkPage--list">
+          <li>
+            <div className="bookMarkPage--thumb">
+              <img src={noImage} alt="noImage" />
+            </div>
+            <div className="bookMarkPage--contWrap">
+              <p className="bookMarkPage--name">뉴스제목</p>
+              <p className="bookMarkPage--cont">
+                뉴스내용 입니다. 뉴스내용 입니다. 뉴스내용 입니다. 뉴스내용
+                입니다. 뉴스내용 입니다. 뉴스내용 입니다. 뉴스내용 입니다.
+                뉴스내용 입니다.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
