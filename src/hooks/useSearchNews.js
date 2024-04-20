94b53a153432a9bdf283cdc43ac8905bc91d8f23
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-20 14:10:19 +0900

    conflict fix

--- src/hooks/useSearchNews.js
+++ src/hooks/useSearchNews.js
@@ -1,19 +1,26 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import api from "../utils/api";
 
 const API_KEY = process.env.REACT_APP_KEY;
 
-const fetchSearchNews = async ({ keyword }) => {
+const fetchSearchNews = async ({ keyword, page }) => {
   const response = await api.get(
-    `/top-headlines?q=${keyword}&country=kr&apiKey=${API_KEY}`,
+    `/everything?q=${keyword}&apiKey=${API_KEY}&pageSize=5&page=${page}`,
   );
   return response.data;
 };
 // eslint-disable-next-line
 export const useSearchNewsQuery = ({ keyword }) => {
-  return useQuery({
+  return useInfiniteQuery({
     queryKey: ["searchNews", keyword],
-    queryFn: () => fetchSearchNews({ keyword }),
+    queryFn: ({ pageParam = 1 }) =>
+      fetchSearchNews({ keyword, page: pageParam }),
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.totalResults > pages.length * 5) {
+        return pages.length + 1;
+      }
+      return undefined;
+    },
     onError: error => {
       // eslint-disable-next-line
       console.error("Error fetching search news:", error);
