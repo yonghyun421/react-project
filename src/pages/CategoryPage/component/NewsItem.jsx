13fd7d9d5b153e22171a9fc070a6f3bf6a535bc4
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-20 14:06:07 +0900

    # This is a combination of 11 commits. # This is the 1st commit message:
    conflict fix
    
    # This is the commit message #2:
    
    작업 영역 합치기
    
    # This is the commit message #3:
    
    헤더, 디테일 css 수정
    
    # This is the commit message #4:
    
    footer ui 추가
    
    # This is the commit message #5:
    
    뉴스 상세페이지 작업 수정
    
    # This is the commit message #6:
    
    마이페이지 파일 추가
    
    # This is the commit message #7:
    
    ESlint 오류 수정
    
    # This is the commit message #8:
    
    [ADD] 로그인, 회원가입 페이지 추가
    
    # This is the commit message #9:
    
    뉴스 카테고리 작업 추가
    
    # This is the commit message #10:
    
    [MODIFY] lint fix
    
    # This is the commit message #11:
    
    [ADD] 회원가입 기능 추가

--- src/pages/CategoryPage/component/NewsItem.jsx
+++ src/pages/CategoryPage/component/NewsItem.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-const { categoryName } = useParams();
+import { useParams } from "react-router-dom";
 
-const NewsItem = ({ title, description, url, publishedAt, urlToImage }) => (
-  <div className="news-item">
-    {urlToImage && <img src={urlToImage} alt="News" />}
-    <h2>
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        {title}
-      </a>
-    </h2>
-    <p>{description}</p>
-    <span>{new Date(publishedAt).toLocaleDateString()}</span>
-  </div>
-);
+const { categoryName } = useParams;
+
+function NewsItem({ title, description, url, publishedAt, urlToImage }) {
+  return (
+    <div className="news-item">
+      {urlToImage && <img src={urlToImage} alt="News" />}
+      <h2>
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          {title}
+        </a>
+      </h2>
+      <p>{description}</p>
+      <span>{new Date(publishedAt).toLocaleDateString()}</span>
+    </div>
+  );
+}
 
 export default NewsItem;
