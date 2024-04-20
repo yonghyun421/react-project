7f1fbbe424c498e9e17b816dc14a5111de3bbdb1
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-20 13:35:18 +0900

    spinner 추가

--- src/pages/Homepage/components/CategoryNewsSlide/CategoryNewsSlide.jsx
+++ src/pages/Homepage/components/CategoryNewsSlide/CategoryNewsSlide.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Alert } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import NewsSlider from "../../../../common/NewsSlider/NewsSlider";
 import { useCategoryNews } from "../../../../hooks/useCategoryNews";
+import LoadingSpinner from "../../../../common/LoadingSpinner/LoadingSpinner";
 
 function CategoryNewsSlide({ category }) {
   const { data, isError, isLoading, error } = useCategoryNews(category);
+  const navigate = useNavigate();
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <Alert variant="danger">{error.message}</Alert>;
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) {
+    navigate("/error");
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+
   return (
     <div>
       <NewsSlider
