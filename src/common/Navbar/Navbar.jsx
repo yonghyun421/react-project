a4663a88b7a244483ed477ca3fc4684e7386ba69
JuYoung <rlaguswls9838@icloud.com>
2024-04-20 21:53:20 +0900

    Header 완성

--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.style.css";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { ReactComponent as CloseIcon } from "../../assets/close-icon.svg";
 
 const categories = [
   "business",
@@ -22,36 +23,43 @@ const categoryNames = {
   sports: "스포츠",
   technology: "기술",
 };
-
+/* eslint-disable react/prop-types */
 function NavigationBar(props) {
-  const { isMenuIconVisible } = props;
+  const { isMenuIconVisible, handleMenuIconClick } = props;
   return (
-    <div className={`navbar-container ${isMenuIconVisible ? "open" : ""}`}>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="light"
-        variant="light"
-        className="top-navbar">
-        <div>
-          <Nav className="flex-row">
-            {/* eslint-disable react/no-array-index-key */}
-            {categories.map((category, index) => (
-              <Nav.Item key={index} className="list-contain">
-                <Nav.Link
-                  as={NavLink}
-                  to={`/news/category/${category}`}
-                  className={({ isActive }) =>
-                    `cate
-                    gories-list ${isActive ? "active-link" : ""}`
-                  }>
-                  {categoryNames[category]}
-                </Nav.Link>
-              </Nav.Item>
-            ))}
-          </Nav>
-        </div>
-      </Navbar>
+    <div>
+      <div className={`navbar-container ${isMenuIconVisible ? "open" : ""}`}>
+        <button type="button" className="dim" onClick={handleMenuIconClick}>
+          뒷배경
+        </button>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="light"
+          variant="light"
+          className="top-navbar">
+          <div className="close-btn">
+            <CloseIcon onClick={handleMenuIconClick} />
+          </div>
+          <div className="category-list">
+            <Nav className="flex-row">
+              {/* eslint-disable react/no-array-index-key */}
+              {categories.map((category, index) => (
+                <Nav.Item key={index} className="list-contain">
+                  <Nav.Link
+                    as={NavLink}
+                    to={`/news/category/${category}`}
+                    className={({ isActive }) =>
+                      `categories-list ${isActive ? "active-link" : ""}`
+                    }>
+                    {categoryNames[category]}
+                  </Nav.Link>
+                </Nav.Item>
+              ))}
+            </Nav>
+          </div>
+        </Navbar>
+      </div>
     </div>
   );
 }
