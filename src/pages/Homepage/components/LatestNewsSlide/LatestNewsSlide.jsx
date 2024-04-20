a857335013c67e76a3e9a7c127923b71165df484
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-20 13:35:18 +0900

    spinner 추가

--- src/pages/Homepage/components/LatestNewsSlide/LatestNewsSlide.jsx
+++ src/pages/Homepage/components/LatestNewsSlide/LatestNewsSlide.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Alert } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import NewsSlider from "../../../../common/NewsSlider/NewsSlider";
 import { useLatestNews } from "../../../../hooks/useLatestNews";
+import LoadingSpinner from "../../../../common/LoadingSpinner/LoadingSpinner";
 
 function LatestNewsSlide() {
   const { data, isError, isLoading, error } = useLatestNews();
+  const navigate = useNavigate();
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <Alert variant="danger">{error.message}</Alert>;
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) {
+    navigate("/error");
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
   return (
     <div>
       <NewsSlider
