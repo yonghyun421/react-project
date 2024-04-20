112a41ca0a810b458c7f80941103c9bfe626822f
JuYoung <rlaguswls9838@icloud.com>
2024-04-20 18:18:19 +0900

    Merge

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, forwardRef } from "react";
 import "./Header.css";
-
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link, useNavigate } from "react-router-dom";
@@ -8,6 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
 import { ReactComponent as MenuIcon } from "../../assets/menu-icon.svg";
 import authenticateAction from "../../redux/acticon/authenticateAction";
+import NavigationBar from "../Navbar/Navbar";
 
 const Header = forwardRef((props, ref) => {
   const dispatch = useDispatch();
@@ -15,6 +15,7 @@ const Header = forwardRef((props, ref) => {
   const [keyword, setKeyword] = useState("");
   const [isSearchBoxVisible, setIsSearchBoxVisible] = useState(false);
   const navigate = useNavigate();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const searchByKeyword = event => {
     event.preventDefault();
@@ -75,25 +76,56 @@ const Header = forwardRef((props, ref) => {
             검색
             <SearchIcon />
           </div>
-          <Form
-            className={`d-flex search--box ${
-              isSearchBoxVisible ? "" : "hidden"
-            }`}
-            onSubmit={searchByKeyword}>
-            <Form.Control
-              type="search"
-              placeholder="SEARCH"
-              className="me-2"
-              aria-label="Search"
-              value={keyword}
-              onChange={event => setKeyword(event.target.value)}
-            />
-            <Button type="submit">
+        </div>
+        <div className="logo">
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            NewsTab
+          </Link>
+        </div>
+        <div className="util--Btn">
+          <div className="mypage--btn">
+            <Link as={Link} to="/mypage/login" className="text-white">
+              로그인
+            </Link>
+            <Link as={Link} to="/movies" className="text-white">
+              마이페이지
+            </Link>
+          </div>
+          <div className="search--btn">
+            <div
+              role="button"
+              tabIndex={0}
+              onClick={handleSearchIconClick}
+              className={`search--icon ${isSearchBoxVisible ? "hidden" : ""}`}
+              onKeyPress={e => {
+                if (e.key === "Enter") {
+                  handleSearchIconClick();
+                }
+              }}>
+              검색
               <SearchIcon />
-            </Button>
-          </Form>
+            </div>
+            <Form
+              className={`d-flex search--box ${
+                isSearchBoxVisible ? "" : "hidden"
+              }`}
+              onSubmit={searchByKeyword}>
+              <Form.Control
+                type="search"
+                placeholder="SEARCH"
+                className="me-2"
+                aria-label="Search"
+                value={keyword}
+                onChange={event => setKeyword(event.target.value)}
+              />
+              <Button type="submit">
+                <SearchIcon />
+              </Button>
+            </Form>
+          </div>
         </div>
       </div>
+      <NavigationBar isSidebarOpen={isSidebarOpen} />
     </div>
   );
 });
