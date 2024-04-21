61fa2d1ab1d1c380c6a31c8104e4980971886b6c
eychoi <atelier2430@gmail.com>
2024-04-21 16:15:42 +0900

    [UPDATE] 유투브 api 교체 및 hook 수정

--- src/hooks/useSearchYoutube.js
+++ src/hooks/useSearchYoutube.js
@@ -3,8 +3,17 @@ import apiYoutubeData from "../utils/apiYoutubeData";
 
 const fetchYoutubes = ({ keyword }) =>
   keyword
-    ? apiYoutubeData.get(`/search`, { params: { q: keyword, maxResults: 10 } })
-    : apiYoutubeData.get(`/search`, { params: { maxResults: 10 } });
+    ? apiYoutubeData.get(`/search`, {
+        params: { q: keyword, maxResults: 10, part: "snippet" },
+      })
+    : apiYoutubeData.get(`/videos`, {
+        params: {
+          chart: "mostPopular",
+          regionCode: "KR",
+          part: "snippet",
+          maxResults: 10,
+        },
+      });
 
 const useSearchYoutubeQuery = ({ keyword }) =>
   useQuery({
