82437190aa897518df92bcc815e1acc26d8c63ab
eychoi <atelier2430@gmail.com>
2024-04-21 17:01:34 +0900

    [FIX] Private Route Props 추가

--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Navigate } from "react-router-dom";
 import Mypage from "../pages/Mypage/Mypage";
 
-function PrivateRoute() {
-  // eslint-disable-next-line
-  const isAuthenticated = localStorage.getItem("authenticate") === "true";
-
-  return isAuthenticated ? <Mypage /> : <Navigate to="/login" />;
+function PrivateRoute({ isLogin }) {
+  return isLogin ? <Mypage /> : <Navigate to="/login" />;
 }
+
+PrivateRoute.propTypes = {
+  isLogin: PropTypes.bool,
+};
+
+PrivateRoute.defaultProps = {
+  isLogin: false,
+};
 
 export default PrivateRoute;
